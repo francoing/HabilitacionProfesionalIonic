e5b59a9f043929215bdf5697a529b6010181b3e7
maxida <maximo_k2002@hotmail.com>
2020-05-26 00:53:40 -0300

    cuarto commit Max

--- src/pages/ubicacion/ubicacion.ts
+++ src/pages/ubicacion/ubicacion.ts
@@ -1,25 +1,40 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the UbicacionPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import leaflet from 'leaflet';
+ 
 @Component({
-  selector: 'page-ubicacion',
-  templateUrl: 'ubicacion.html',
+  selector: 'ubicacion-home',
+  templateUrl: 'ubicacion.html'
 })
 export class UbicacionPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
+  constructor(public navCtrl: NavController) {
+ 
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UbicacionPage');
+ 
+  ionViewDidEnter() {
+    this.loadmap();
   }
+ 
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'www.tphangout.com',
+      maxZoom: 18
+    }).addTo(this.map);
+    this.map.locate({
+    setView: true,
+    maxZoom: 10
+  }).on('locationfound', (e) => {
+    let markerGroup  = leaflet.featureGroup();
+    let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
+      alert('Marker clicked')
+    })
+    markerGroup.addLayer(marker);
+    this.map.addLayer(markerGroup);
+  })
 
+  }
 }
+ 
